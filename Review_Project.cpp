1256fcfd2d192ceae7e5db4d3a58fa685a49aaec
Tcubstead <tec44912@email.vccs.edu>
2025-02-10 11:01:01 -0500

    created functions to get the user choice and to determine a winner after selection

--- Review_Project.cpp
+++ Review_Project.cpp
@@ -5,11 +5,48 @@
 //This program will prompt a user to play a gmae of rock paper scissors and track the score until the user is done then a winner will be displayed based on the score.
 
 #include <iostream>
+#include <random>
+#include <string>
 using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+//get the user input and stores it in choice
+string userChoice() {
+	string choice;
+	while (true) {
+		cout << "enter rock, paper, Scissors, or quit" << endl;
+		cin >> choice;
+		//turn choice lowercase for code flow
+		for (auto & c : choice) c = tolower(c);
+		if (choice == 'rock' || choice == 'paper' || choice == 'scissors' || choice == 'quit') {
+			return choice;
+		} else {
+			cout << "you must input rock, paper, scissors, or quit, please try again" << endl;
+		}
+	}
+}
+
+//determines the winner based on the selection of user and computer
+string winSelection(string userChoice, string computerChoice) {
+	if (userChoice == computerChoice) {
+		return "draw";
+	}
+	if ((userChoice == "rock" && computerChoice == "scissors") || (userChoice == "paper" && computerChoice == "rock") || (userChoice == "scissors" && computerChoice == "paper")) {
+		return "YOU WIN!";
+	} else {
+		return "COMPUTER WINS!";
+	}
+}
+
+int main() {
+	//max and min values to simulated rock paper scissor values
+	const int MIN = 1;
+	const int MAX = 3;
+
+	random_device engine;
+
+	uniform_int_distribution<int>rValue(MIN, MAX);
+	cout << rValue(engine) << endl;
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
